666f10ca2ca145aa961cbc6754f3d4b8a6022eb5
Jacob Borch Madsen <jacob.borch@outlook.com>
2024-03-04 11:41:12 +0100

    added todo list

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,6 @@ pub mod suduko;
 
 pub type Grid = Vec<Vec<u8>>;
 
+pub trait Solver {
+    fn solve(&self) -> Option<Grid>;
+}
--- src/suduko.rs
+++ src/suduko.rs
@@ -2,20 +2,22 @@ use std::collections::HashSet;
 
 use crate::{
     grid::{GridLoader, TextFileReader},
-    Grid,
+    Grid, Solver,
 };
 
-pub struct Solver {
+pub struct BruteforceSolver {
     grid: Grid,
 }
 
-impl Solver {
+impl Solver for BruteforceSolver {
+    fn solve(&self) -> Option<Grid> {
+        Self::aux_solver(&mut self.grid.clone())
+    }
+}
+
+impl BruteforceSolver {
     pub fn new(grid: Grid) -> Self {
         Self { grid }
-    }
-
-    pub fn solve(&self) -> Option<Grid> {
-        Self::aux_solver(self.grid.clone())
     }
 
     fn extract_square(grid: &Grid, no: usize) -> Vec<u8> {
@@ -36,38 +38,41 @@ impl Solver {
         grid.iter().map(|row| row[j]).collect()
     }
 
-    fn aux_solver(mut grid: Grid) -> Option<Grid> {
+    fn aux_solver(grid: &mut Grid) -> Option<Grid> {
         if !Self::is_valid(&grid) {
             return None;
         }
         let next_entry = Self::next_entry_to_change(&grid);
         if next_entry.is_none() {
-            return Some(grid);
+            return Some(grid.clone());
         }
         let (i, j) = next_entry.unwrap();
         for v in 1..=9 {
             grid[i][j] = v;
-            let res = Self::aux_solver(grid.clone());
+            let res = Self::aux_solver(grid);
             if res.is_some() {
                 return res;
             }
+            grid[i][j] = 0;
         }
         None
     }
     //Returns true if the row has any duplicate values (other than zero)
     fn has_duplicate(row: &Vec<u8>) -> bool {
-        let it = row.iter().filter(|&&x| x != 0);
-        it.clone().collect::<HashSet<_>>().len() != it.collect::<Vec<_>>().len()
+        //let it = row.iter().filter(|&&x| x != 0);
+        row.iter().filter(|&&x| x != 0).collect::<HashSet<_>>().len() 
+        != 
+        row.iter().filter(|&&x| x != 0).collect::<Vec<_>>().len()
     }
 
     fn is_valid(grid: &Grid) -> bool {
-        grid.iter().all(|row| !Solver::has_duplicate(row))
+        grid.iter().all(|row| !BruteforceSolver::has_duplicate(row))
             && (0..9)
                 .map(|j| Self::extract_col(grid, j))
-                .all(|col| !Solver::has_duplicate(&col))
+                .all(|col| !BruteforceSolver::has_duplicate(&col))
             && (0..9)
                 .map(|j| Self::extract_square(grid, j))
-                .all(|square| !Solver::has_duplicate(&square))
+                .all(|square| !BruteforceSolver::has_duplicate(&square))
     }
 
     fn next_entry_to_change(grid: &Grid) -> Option<(usize, usize)> {
@@ -89,66 +94,66 @@ lazy_static::lazy_static! {
 
 #[test]
 fn test_next_entry() {
-    let next_entry = Solver::next_entry_to_change(&GRID);
+    let next_entry = BruteforceSolver::next_entry_to_change(&GRID);
     assert_eq!(next_entry, Some((0, 1)))
 }
 
 #[test]
 fn has_duplicate_with_duplicate() {
     let row = vec![0, 1, 2, 0, 1, 0, 0, 0, 0];
-    let has_duplicate = Solver::has_duplicate(&row);
+    let has_duplicate = BruteforceSolver::has_duplicate(&row);
     assert!(has_duplicate)
 }
 
 #[test]
 fn has_duplicate_with_no() {
     let row = vec![0, 1, 2, 0, 3, 0, 5, 0, 8];
-    let has_duplicate = Solver::has_duplicate(&row);
+    let has_duplicate = BruteforceSolver::has_duplicate(&row);
     assert!(!has_duplicate)
 }
 
 #[test]
 fn is_valid_when_valid() {
-    let is_valid = Solver::is_valid(&GRID);
+    let is_valid = BruteforceSolver::is_valid(&GRID);
     assert!(is_valid)
 }
 
 #[test]
 fn invalid_row() {
-    let is_valid = Solver::is_valid(&INVALID_ROW_GRID);
+    let is_valid = BruteforceSolver::is_valid(&INVALID_ROW_GRID);
     assert!(!is_valid)
 }
 
 #[test]
 fn invalid_column() {
-    let is_valid = Solver::is_valid(&INVALID_COLUMN_GRID);
+    let is_valid = BruteforceSolver::is_valid(&INVALID_COLUMN_GRID);
     assert!(!is_valid)
 }
 
 #[test]
 fn invalid_square() {
-    let is_valid = Solver::is_valid(&INVALID_SQUARE_GRID);
+    let is_valid = BruteforceSolver::is_valid(&INVALID_SQUARE_GRID);
     assert!(!is_valid)
 }
 
 #[test]
 fn extract_col_works() {
-    let seventh_col = Solver::extract_col(&GRID, 6);
+    let seventh_col = BruteforceSolver::extract_col(&GRID, 6);
     let expected = vec![0, 3, 0, 0, 0, 0, 0, 0, 0];
     assert_eq!(seventh_col, expected)
 }
 
 #[test]
 fn extract_square_works() {
-    let sixth_square = Solver::extract_square(&INVALID_SQUARE_GRID, 5);
+    let sixth_square = BruteforceSolver::extract_square(&INVALID_SQUARE_GRID, 5);
     let expected = vec![1, 0, 0, 0, 0, 0, 0, 0, 1];
     assert_eq!(sixth_square, expected)
 }
 
 #[test]
 fn test_solve() {
-    let solver = Solver::new(SOLVABLE.clone());
+    let solver = BruteforceSolver::new(SOLVABLE.clone());
     let solution = solver.solve();
-    let is_valid_solution = Solver::is_valid(&solution.unwrap());
+    let is_valid_solution = BruteforceSolver::is_valid(&solution.unwrap());
     assert!(is_valid_solution)
 }
